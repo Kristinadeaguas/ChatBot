f5564adaa94a103e31e37e48739cf04c27f818ea
kristinadeaguas <deaguaskristina@gmail.com>
2025-05-23 15:24:06 -0500

    Primer commit

---
+++ backend/routes/chatroutes.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const { addChat, getChats} = require("../controllers/chatcontroller")
+const router = express.Router()
+
+router.post("/add", addChat)
+router.get("/", getChats)
+
+module.exports = router
+
